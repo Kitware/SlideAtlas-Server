806198b34dd1113e74bc9466a5a5a7d89c976fe7
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2012-09-24 19:28:17 -0400

    Using bootstrap template

--- slideatlas/__init__.py
+++ slideatlas/__init__.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, escape
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
+
+app.config['BOOTSTRAP_USE_MINIFIED'] = False
+
 
 @app.route("/")
 @app.route('/<name>')
-
 def hello(name=None):
     """
     All routes get redirected here 
     - / Says Hello <name>
     - /<name> Says Hello <name>
     """
-    return render_template('hello.thtml', name=name)
+    return render_template('hello.html', name=name)
