7fabcbeec79ca64dde3fc43a7d97ef282fe0f844
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2012-09-24 15:10:11 -0400

    Starting with some documentation

--- slideatlas/__init__.py
+++ slideatlas/__init__.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route('/<name>')
+
 def hello(name=None):
+    """
+    All routes get redirected here 
+    - / Says Hello <name>
+    - /<name> Says Hello <name>
+    """
     return render_template('hello.thtml', name=name)
