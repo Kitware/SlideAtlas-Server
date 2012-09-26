e1c547d07d9e6a702f4fc7000b0cecfdbeb8ee5b
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2012-09-26 14:13:56 -0400

    Skeleton for a session or session-list

--- slideatlas/__init__.py
+++ slideatlas/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, escape, g, request, redirect, session, url_for
+
 from flask.ext.bootstrap import Bootstrap
 from flaskext.openid import OpenID
 
@@ -8,10 +9,55 @@ oid = OpenID(app)
 
 
 app.config['BOOTSTRAP_USE_MINIFIED'] = False
-
 # set the secret key.  keep this really secret:
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
+
+@app.before_request
+def before_request():
+    g.logged_in = False
+
+    # Find out if any openid information in the sesssion 
+    if 'openid' in session:
+        g.logged_in = True
+        #g.user = session['user']
+#        return
+    print "I am here .. in before request"
+
+
+@app.after_request
+def after_request(response):
+    session.pop('openid', None)
+    return response
+
+
+@app.route('/sessions')
+def sessions(name=None):
+    """
+    - /sessions  With no argument displays list of sessions accessible to current user
+    - /sessions?sess=10239094124  searches for the session id 
+    """
+    if 'user' in session:
+        name = session["user"]["name"]
+        email = session["user"]["email"]
+    else:
+        # Send the user back to login page
+        # with some message
+        return redirect(url_for('login'))
+
+    # See if the user is requesting any session id
+    sessid = request.args.get('sess', None)
+    if sessid:
+        # Find and return a single session
+        asession = {}
+        return render_template('session.html', session=asession, name=name)
+
+    else:
+        # Find and return a list of session names / ids
+        sessionlist = []
+        # Array of dictionaries
+
+        return render_template('sessionlist.html', sessions=sessionlist, name=name)
 
 @app.route("/")
 @app.route('/home/<name>')
@@ -29,23 +75,7 @@ def home(name=None):
     else:
         return render_template('hello.html', name=name)
 
-@app.before_request
-def before_request():
-    g.logged_in = False
-    print "Session is here ..", session
 
-    # Find out if any openid information in the sesssion 
-    if 'openid' in session:
-        g.logged_in = True
-        #g.user = session['user']
-#        return
-    print "I am here .. in before request"
-
-
-@app.after_request
-def after_request(response):
-    session.pop('openid', None)
-    return response
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
