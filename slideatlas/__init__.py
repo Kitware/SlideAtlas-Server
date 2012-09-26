ead6cf78db71a463584387d7227fe01483310038
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2012-09-25 23:42:05 -0400

    OpenID authentication working

--- slideatlas/__init__.py
+++ slideatlas/__init__.py
@@ -45,16 +45,32 @@ def login():
     """
     # if we are already logged in, go back to were we came from
     if g.user is not None:
-        app.logger.info('logged-in: '+oid.get_next_url())
+        app.logger.info('logged-in: ' + oid.get_next_url())
         return redirect(oid.get_next_url())
-    if request.method == 'POST':
+    if request.method == 'POST' or request.method == 'GET':
         openid = request.form.get('openid_identifier')
         if openid:
             app.logger.info(request.form)
-            app.logger.info('logging-in: '+oid.get_next_url())
+            app.logger.info('logging-in: ' + oid.get_next_url())
             return oid.try_login(openid, ask_for=['email', 'fullname',
                                                   'nickname'])
     print "I am here "
-    app.logger.info('not-logged-in: '+oid.get_next_url())
+    app.logger.info('not-logged-in: ' + oid.get_next_url())
     return render_template('login.html', next=oid.get_next_url(),
                            error=oid.fetch_error())
+
+
+@oid.after_login
+def create_or_login(resp):
+    """This is called when login with OpenID succeeded and it's not
+    necessary to figure out if this is the users's first login or not.
+    This function has to redirect otherwise the user will be presented
+    with a terrible URL which we certainly don't want.
+    """
+    session['openid'] = resp.identity_url
+    name = resp.fullname or resp.nickname
+    email = resp.email
+    return render_template('hello.html', name=name, email=email)
+
+
+
