91c9a95bdc18b90af6925b3445ec109bb290972c
Brian Helba <brian.helba@kitware.com>
2012-09-26 20:06:29 -0400

    Basic login functionality using digitalpath framework

--- slideatlas/__init__.py
+++ slideatlas/__init__.py
@@ -1,20 +1,28 @@
 from flask import Flask, render_template, escape, g, request, redirect, session, url_for
 
 from flask.ext.bootstrap import Bootstrap
-from flaskext.openid import OpenID
+
+
+import digitalpath
 
 app = Flask(__name__)
 Bootstrap(app)
-oid = OpenID(app)
 
 
 app.config['BOOTSTRAP_USE_MINIFIED'] = False
 # set the secret key.  keep this really secret:
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
+from .views import login
+login.oid.init_app(app)
+app.register_blueprint(login.mod)
+
 
 @app.before_request
 def before_request():
+
+    digitalpath.init_admin_db('zomm', 'slideatlas_test')
+
     g.logged_in = False
 
     # Find out if any openid information in the sesssion 
@@ -22,7 +30,7 @@ def before_request():
         g.logged_in = True
         #g.user = session['user']
 #        return
-    print "I am here .. in before request"
+    #print "I am here .. in before request"
 
 
 @app.after_request
@@ -87,21 +95,11 @@ def sessions(name=None):
         return render_template('sessionlist.html', sessions=sessionlist, name=name)
 
 @app.route("/")
-@app.route('/home/<name>')
-def home(name=None):
+@app.route("/index")
+def index():
     """
-    All routes get redirected here 
-    - / Says Hello <name>
-    - /<name> Says Hello <name>
     """
-    if 'user' in session:
-        name = session["user"]["name"]
-        email = session["user"]["email"]
-        return render_template('hello.html', name=name, email=email)
-#        return render_template('hello.html')
-    else:
-        return render_template('hello.html', name=name)
-
+    return redirect('/login')
 
 
 @app.route('/logout', methods=['GET', 'POST'])
@@ -114,44 +112,5 @@ def logout():
     session.pop('user', None)
     session.pop('openid', None)
     return redirect(url_for('home'))
-
-@app.route('/login', methods=['GET', 'POST'])
-@oid.loginhandler
-def login():
-    """Does the login via OpenID. Has to call into `oid.try_login`
-    to start the OpenID machinery.
-    """
-    # if we are already logged in, go back to were we came from
-    if g.logged_in == True:
-        app.logger.info('logged-in: ' + oid.get_next_url())
-        return redirect(oid.get_next_url())
-    if request.method == 'POST' or request.method == 'GET':
-        openid = request.form.get('openid_identifier')
-        if openid:
-            app.logger.info(request.form)
-            app.logger.info('logging-in: ' + oid.get_next_url())
-            return oid.try_login(openid, ask_for=['email', 'fullname',
-                                                  'nickname'])
-    app.logger.info('not-logged-in: ' + oid.get_next_url())
-    return render_template('login.html', next=oid.get_next_url(),
-                           error=oid.fetch_error())
-
-
-@oid.after_login
-def create_or_login(resp):
-    """This is called when login with OpenID succeeded and it's not
-    necessary to figure out if this is the users's first login or not.
-    This function has to redirect otherwise the user will be presented
-    with a terrible URL which we certainly don't want.
-    """
-    session['openid'] = resp.identity_url
-    name = resp.fullname or resp.nickname
-
-    session['user'] = {'url': resp.identity_url,
-                         'name': name,
-                         'email': resp.email}
-
-    return redirect(url_for('home'))
-
 
 
