07663581b8eaf7da6d1dc00adcd7d1f832b4d424
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2012-09-27 18:14:25 -0400

    Added data model from mongokit

---
+++ slideatlas/model/session.py
@@ -0,0 +1,26 @@
+from mongokit import Document
+from bson import ObjectId
+
+class RefList(Document):
+    structure = {
+        'ref' : ObjectId,
+        'pos' : float,
+        'hide' : bool,
+        'label' : basestring
+        }
+
+    required_fields = [ 'ref', 'pos']
+
+
+
+class Session(Document):
+    structure = {
+        'name' : basestring,
+        'label' : basestring,
+        'images' : [RefList],
+        'attachments' : [RefList]
+        }
+    
+    required_fields = [ 'name']
+
+
