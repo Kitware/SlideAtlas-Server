4c8272e8573d8a6f149acc84488cca0c8dcc8e95
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2012-10-21 21:27:23 -0400

    Facebook login mechanism

--- slideatlas/views/login.py
+++ slideatlas/views/login.py
@@ -1,11 +1,27 @@
 
-from flask import Blueprint, current_app, redirect, render_template, request, session, flash
+from flask import Blueprint, current_app, redirect, render_template, request, session, flash, url_for
 from flaskext.openid import OpenID
+from flask_oauth import OAuth
 
 from .. import digitalpath
 
 mod = Blueprint('login', __name__)
 oid = OpenID()
+oauth = OAuth()
+
+# Facebook key redirecting to localhost:8080
+FACEBOOK_APP_ID = '458796434162322'
+FACEBOOK_APP_SECRET = '32cce59abab133519ccc51470c8b32f4'
+
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': 'email'}
+)
 
 @mod.route("/login")
 def login():
@@ -29,12 +45,38 @@ def login_passwd():
     else:
         return do_user_login(user)
 
-
 @mod.route('/login.facebook')
 def login_facebook():
-    flash('Not implemented yet', 'error')
-    return redirect('/home')
+    return facebook.authorize(callback=url_for('.facebook_authorized',
+        next=request.args.get('next') or request.referrer or None,
+        _external=True))
 
+@mod.route('/facebook_authorized')
+@facebook.authorized_handler
+def facebook_authorized(resp=None):
+    print resp
+    if resp is None:
+        flash('Facebook Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        ), 'error')
+        return redirect(url_for('home'))
+
+    session['oauth_token'] = (resp['access_token'], '')
+    me = facebook.get('/me')
+    session['user'] = {
+        'id': "Not_yet",
+        'label': me.data['name'],
+        'facebook_id': me.data['id']
+        }
+    flash('Logged in as id=%s name=%s redirect=%s' % \
+          (me.data['id'], me.data['name'], request.args.get('next')), "success")
+
+    return redirect(url_for('home'))
+
+@facebook.tokengetter
+def get_facebook_oauth_token():
+    return session.get('oauth_token')
 
 @mod.route('/login.google')
 @oid.loginhandler
