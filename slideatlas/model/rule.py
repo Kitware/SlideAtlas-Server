6aa4665a08a20b89bebfedb80b3082da900cd363
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2012-10-22 17:00:04 -0400

    Code to grant access to kawai1 db sessions

--- slideatlas/model/rule.py
+++ slideatlas/model/rule.py
@@ -9,7 +9,8 @@ class Rule(mongokit.Document):
         'db' : ObjectId,
         'facebook_id' : basestring,
         'can_see' : [ObjectId],
-        'can_see_all' : bool
+        'can_see_all' : bool,
+        'db_admin' : bool
     }
 
 required_fields = ['label', 'db']
