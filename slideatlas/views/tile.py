469d2a3b5863960ccabb61114e617f1d0249a9bd
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2012-10-23 00:05:02 -0400

    login protection for tile

--- slideatlas/views/tile.py
+++ slideatlas/views/tile.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Response, abort, request
+from flask import Blueprint, Response, abort, request, session
 from slideatlas.connections import slconn as conn
 from bson import ObjectId
 from slideatlas import model
@@ -10,12 +10,17 @@ def tile():
     """
     - /tile/4e695114587718175c000006/t.jpg  searches and returns the image
     """
+    # Get variables
     img = request.args.get('img', None)
     db = request.args.get('db', None)
     name = request.args.get('name', None)
+
+    if not 'user' in session:
+        abort(403)
+
     conn.register([model.Database])
-    admindb = conn['slideatlasv2']
-    dbobj = admindb["databases"].Database.find_one({"_id" : ObjectId(db)})
+    admindb = conn["slideatlasv2"]
+    dbobj = admindb["databases"].find_one({"_id" : ObjectId(db)})
     imgdb = conn[dbobj['dbname']]
     colImage = imgdb[img]
     docImage = colImage.find_one({'name':name})
