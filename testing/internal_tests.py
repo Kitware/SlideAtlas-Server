ee6a5e351998e5a987bd8b04f10b5601b2e0de10
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2012-11-12 14:26:04 -0500

    Skeleton for internal tests

---
+++ testing/internal_tests.py
@@ -0,0 +1,22 @@
+import os
+import sys
+sys.path.append("..")
+from slideatlas import app
+import unittest
+import tempfile
+
+class SlideAtlasBasicTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def login(self):
+        pass
+
+    def test_login_logout(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
