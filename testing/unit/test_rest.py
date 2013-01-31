04746e22d5f4e3c74e053f8c6f9f344357f0dcc2
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2013-01-31 16:06:50 -0500

    Adding login / logout in tests

--- testing/unit/test_rest.py
+++ testing/unit/test_rest.py
@@ -10,6 +10,15 @@ class APIv1_Tests(unittest.TestCase):
         self.app1.testing = True
         self.app = self.app1.test_client()
 
+    def login(self):
+        return self.app.post('/login.passwd', data=dict(
+            username="all_demo",
+            passwd=""
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
     def testRestURLs(self):
         urls_to_pass = [  "/apiv1/5074589002e31023d4292d83/sessions",
                                     "/apiv1/5074589002e31023d4292d83/sessions/5074589002e31023d4292d83",
@@ -24,8 +33,9 @@ class APIv1_Tests(unittest.TestCase):
         urls_to_fail = ["/apiv1/somethingelse"]
 
         for aurl in urls_to_pass:
-            print "Testing: ", aurl
+            print "Testing: ",
             rv = self.app.get(aurl)
+            print rv.status_code, " ", aurl
             assert rv.status_code != 404
 
         # Now test for urls that should not pass
@@ -34,10 +44,23 @@ class APIv1_Tests(unittest.TestCase):
             rv = self.app.get(aurl)
             assert rv.status_code == 404
 
-    def testLogin(self):
+    def testLoginWithUser(self):
         """ Any URL should not return without logging in
         """
-        pass
+
+        aurl = "/apiv1/5074589002e31023d4292d83/sessions/5074589002e31023d4292d83"
+        aurl = "/apiv1/databases/5074589002e31023d4292d83"
+
+        # expact 401
+        rv = self.app.get(aurl)
+        print "Before login: ", rv.status_code, " ", aurl
+        assert rv.status_code == 401
+
+        # expact 200
+        self.login()
+        rv = self.app.get(aurl)
+        print "After login : ", rv.status_code, " ", aurl
+        assert rv.status_code == 200
 
 
 
