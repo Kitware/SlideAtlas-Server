6fb8c2fad374e0014070ea54dd1f0d3271bbe422
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2013-01-31 14:32:02 -0500

    Testing url mapping

---
+++ testing/unit/test_rest.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append("../..")
+sys.path.append("..")
+import slideatlas
+import unittest
+
+class APIv1_Tests(unittest.TestCase):
+    def setUp(self):
+        self.app = slideatlas.app.test_client()
+
+    def testRestURLs(self):
+        urls_to_test = [  "/apiv1/5074589002e31023d4292d83/sessions",
+                                    "/apiv1/5074589002e31023d4292d83/sessions/5074589002e31023d4292d83",
+                                    "/apiv1/5074589002e31023d4292d83/sessions/5074589002e31023d4292d83/views",
+                                    "/apiv1/5074589002e31023d4292d83/sessions/5074589002e31023d4292d83/views/5074589002e31023d4292d83",
+                                    "/apiv1/5074589002e31023d4292d83/sessions/5074589002e31023d4292d83/attachments",
+                                    "/apiv1/5074589002e31023d4292d83/sessions/5074589002e31023d4292d83/attachments/5074589002e31023d4292d83",
+                                    "/apiv1/users/5074589002e31023d4292d83",
+                                    "/apiv1/databases",
+                                    "/apiv1/rules/5074589002e31023d4292d83"]
+
+        for aurl in urls_to_test:
+            print "Testing: ", aurl
+            rv = self.app.get(aurl)
+            assert 'want' in rv.data
+
+if __name__ == "__main__":
+    unittest.main()
