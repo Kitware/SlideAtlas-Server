b110a520365355cff1736e772a4a45e97295fbb0
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2013-01-31 14:49:16 -0500

    Checking return code in tests

--- testing/unit/test_rest.py
+++ testing/unit/test_rest.py
@@ -6,7 +6,9 @@ import unittest
 
 class APIv1_Tests(unittest.TestCase):
     def setUp(self):
-        self.app = slideatlas.app.test_client()
+        self.app1 = slideatlas.app
+        self.app1.testing = True
+        self.app = self.app1.test_client()
 
     def testRestURLs(self):
         urls_to_test = [  "/apiv1/5074589002e31023d4292d83/sessions",
@@ -17,12 +19,13 @@ class APIv1_Tests(unittest.TestCase):
                                     "/apiv1/5074589002e31023d4292d83/sessions/5074589002e31023d4292d83/attachments/5074589002e31023d4292d83",
                                     "/apiv1/users/5074589002e31023d4292d83",
                                     "/apiv1/databases",
-                                    "/apiv1/rules/5074589002e31023d4292d83"]
+                                    "/apiv1/rules/5074589002e31023d4292d83",
+                                    "/apiv1/somethingelse"]
 
         for aurl in urls_to_test:
             print "Testing: ", aurl
             rv = self.app.get(aurl)
-            assert 'want' in rv.data
+            assert rv.status_code != 404
 
 if __name__ == "__main__":
     unittest.main()
