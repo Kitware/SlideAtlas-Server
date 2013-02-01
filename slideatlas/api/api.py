a9f8626e6f8e73feaa9d5cfbe52813c78dc60be6
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2013-01-31 23:11:21 -0500

    Url rule for session objects

--- slideatlas/api/api.py
+++ slideatlas/api/api.py
@@ -32,40 +32,50 @@ class AdminDBAPI(MethodView):
 
     def post(self, restype):
         # create a new user
+        if restype == "rules":
+                return "You want to post rule"
+        elif restype == 'databases':
+                return "You want to add database"
+        elif restype == 'users':
+            # Posting to users means typically adding new rules to users
+            return "You want to add database"
+
         pass
 
     def delete(self, restype, resid):
-        # delete a single user
+        # Verify the access
+        # Remove one instance
+        # and remove the given resource
+        # Not implemented right now
         pass
 
     def put(self, restype, resid):
-        # update a single user
+        # update some information
         pass
 
 mod.add_url_rule('/<regex("(databases|users|rules)"):restype>', defaults={"resid" : None}, view_func=AdminDBAPI.as_view("show_resources"))
 mod.add_url_rule('/<regex("(databases|users|rules)"):restype>/<regex("[a-f0-9]{24}"):resid>', view_func=AdminDBAPI.as_view("show_resources"))
 
-#@mod.route('/<regex("(databases|users|rules)"):restype>', defaults={"resid" : None})
-#@mod.route('/<regex("(databases|users|rules)"):restype>'
-#                        '/<regex("[a-f0-9]{24}"):resid>')
-##@mod.route('/<regex("[a-f0-9]{24}"):resid>', defaults={"restype" : None})
-#def admin_db_request(restype, resid):
-#    """
-#        /apiv1/users/5074589002e31023d4292d83
-#        /apiv1/databases
-#        /apiv1/rules/5074589002e31023d4292d83
-#    """
-#
-#    # See if the user is requesting any session id
-#    return "you want : %s, %s" % (restype, resid)
+
+# The url valid for databases, rules and users with supported queries
+class DataSessionItemsAPI(MethodView):
+    decorators = [common_utils.user_required]
+
+    def get(self, dbid, sessid, restype, resid):
+        if resid == None:
+            return "You want alist of %s/%s/%s" % (dbid, sessid, restype)
+        else:
+            if restype == "attachments":
+                return "You want list of attachments in %s/%s" % (dbid, sessid)
+            else:
+                return "You want list of views in %s/%s" % (dbid, sessid)
 
 
-
-
-
-
-
-
+# For a list of resources within session
+mod.add_url_rule('/<regex("[a-f0-9]{24}"):dbid>'
+                                '/sessions'
+                                '/<regex("[a-f0-9]{24}"):sessid>'
+                                '/<regex("(attachments|views)"):restype>', view_func=DataSessionItemsAPI.as_view("show_session_item_list"), defaults={"resid" : None})
 
 # Specially for session
 
@@ -79,12 +89,12 @@ mod.add_url_rule('/<regex("(databases|users|rules)"):restype>/<regex("[a-f0-9]{2
                         '/<regex("[a-f0-9]{24}"):sessid>', defaults={"resid" : None, "restype" : None})
 
 # For a list of resources within session
-@mod.route('/<regex("[a-f0-9]{24}"):dbid>'
-                        '/sessions'
-                        '/<regex("[a-f0-9]{24}"):sessid>'
-                        '/<regex("(attachments|views)"):restype>', defaults={"resid" : None})
+#@mod.route('/<regex("[a-f0-9]{24}"):dbid>'
+#                        '/sessions'
+#                        '/<regex("[a-f0-9]{24}"):sessid>'
+#                        '/<regex("(attachments|views)"):restype>', defaults={"resid" : None})
 
-# For a list of resources within session
+# For a particular resource within session
 @mod.route('/<regex("[a-f0-9]{24}"):dbid>'
                         '/sessions'
                         '/<regex("[a-f0-9]{24}"):sessid>'
--- testing/unit/test_rest.py
+++ testing/unit/test_rest.py
@@ -47,7 +47,6 @@ class APIv1_Tests(unittest.TestCase):
     def testLoginWithUser(self):
         """ Any URL should not return without logging in
         """
-106
         # aurl = "/apiv1/5074589002e31023d4292d83/sessions/5074589002e31023d4292d83"
         aurl = "/apiv1/databases/5074589002e31023d4292d83"
 
