06c700fcb88ea968fcc1ade4d41300d21014672a
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2013-02-14 14:17:56 -0500

    Fix for get git version

--- slideatlas/version.py
+++ slideatlas/version.py
@@ -1,15 +1,21 @@
+import os
 from subprocess import check_output
 
 def get_git_name():
+    curdir = os.getcwd()
+    newdir = os.path.dirname(os.path.abspath(__file__))
     out = ''
     try:
         params = ["git", "describe", "--tags", "--always"]
         out = check_output(params)
     except:
         # Support for particular case when running on windows 
-        params = ["C:/PortableGit-1.7.11/bin/git", "describe", "--tags"]
+        os.chdir(newdir)
+        params = ["C:/PortableGit-1.7.11/git", "describe", "--tags"]
         out = check_output(params)
+        os.chdir(curdir)
 
+    os.chdir(newdir)
     return out
 
 if __name__ == "__main__":
