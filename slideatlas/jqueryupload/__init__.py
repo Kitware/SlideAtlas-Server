f15093e4b3d364a5e30ad2d513558cfcfcdbb6d2
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2013-02-19 18:08:51 -0500

    Seems to get chunked requests in firefox

--- slideatlas/jqueryupload/__init__.py
+++ slideatlas/jqueryupload/__init__.py
@@ -3,6 +3,7 @@
 from flask import Blueprint, Response, abort, request, session, render_template, current_app, jsonify
 from werkzeug import secure_filename
 import os
+import re
 
 mod = Blueprint('upload', __name__,
                 template_folder="templates",
@@ -19,6 +20,26 @@ def upload():
         # we are expected to save the uploaded file and return some infos about it:
         #                              vvvvvvvvv   this is the name for input type=file
         names = []
+#        print request.headers
+
+        jsonresponse = {}
+        jsonresponse["files"] = []
+
+        # Construct a json and send it out as response 
+        obj = {}
+        obj["boundary"] = request.headers["Content-Type"]
+        obj["size"] = False
+        obj["delete_type"] = "DELETE"
+
+        # Parse filename from content disposition 
+        fnameheader = request.headers["Content-Disposition"]
+        disposition = re.search(r'filename="(.+?)"', fnameheader)
+        filename = disposition.group(0)[10:-1]
+        obj["delete_url"] = "?file=" + filename
+        obj["name"] = filename
+        print obj
+
+        return jsonify(obj)
 
         for file in request.files.getlist('files[]'):
             filename = secure_filename(file.filename)
