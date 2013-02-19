1ca627336663e5aecab83c0724a7f77ecc74d33e
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2013-02-19 12:45:51 -0500

    Imported code from digitalpath-utils to fix thumbnails after pyramids are built

---
+++ Utils/fix_thumbs.py
@@ -0,0 +1,74 @@
+from __future__ import absolute_import
+import pymongo
+from bson import ObjectId
+import argparse
+import sys
+import os
+
+sys.path.append(os.path.abspath(".."))
+from slideatlas.common_utils import get_object_in_collection
+
+# def CreateJpegThumb(binary_jpeg_image, channel_threshold=245):
+# TODO: Currently depends on wx, this routine should be rewritten 
+from create_thumb import CreateJpegThumb
+
+# Accept DB and SESSION and recreate thumbs if t.jpg exists
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Utility to regenerate thumb nails')
+    parser.add_argument('db' , help='Database instance from which to read')
+    parser.add_argument('session' , help='Session ID or name to synchronize')
+    parser.add_argument("-m", "--mongo" , help='MongoDB Server from which to read', default="slide-atlas.org")
+    parser.add_argument('-n', '--no-op', help='Dry run, no updates to the destination database', action='store_true')
+    parser.add_argument('-f', '--force', help='Entirely removes the session and re-creates', action='store_true')
+    parser.add_argument('-d', '--debug', help='Print more output useful for debugging (default:%(default)s)', default=False, action='store_true')
+    parser.set_defaults(debug=False)
+
+    args = parser.parse_args()
+
+    # TODO: Currently hardcodes the authentication, should use parameters from slideatlas
+    try:
+        # Try opening the database
+        conn = pymongo.Connection(args.mongo)
+        admindb = conn["admin"]
+        if admindb.authenticate("slideatlasweb", "2&PwRaam4Kw") == 0:
+            print "Auth error"
+            raise 1
+
+        db = conn[args.db]
+
+    except:
+        print "Error opening ", args.db
+        sys.exit(0)
+
+    # List all the images in the session
+    colviews = db["views"]
+    colsessions = db["sessions"]
+
+    sessionobj = get_object_in_collection(colsessions, args.session)
+
+    if sessionobj == None:
+        print "Error opening ", args.session
+        sys.exit(0)
+
+#    print "Found session:", sessionobj["label"], "Contains: ", sessionobj.keys()
+
+    for aview in sessionobj["views"]:
+        viewobj = get_object_in_collection(colviews, aview["ref"])
+        img_name = viewobj["img"]
+        print "Working with: ", img_name,
+
+        colimage = db[str(img_name)]
+        tjpeg = get_object_in_collection(colimage, "t.jpg")
+#        print tjpeg["name"]
+
+        thumb = CreateJpegThumb(tjpeg["file"])
+
+        thumbobj = get_object_in_collection(colimage, "thumb.jpg")
+        if thumbobj == None:
+            print "Creating Thumb"
+            colimage.insert({"name" : "thumb.jpg", 'file' : thumb})
+        else:
+            print "Updating Thumb"
+            colimage.update({"_id" : thumbobj["_id"]}, {"$set" : { 'file' : thumb}})
+
--- slideatlas/common_utils.py
+++ slideatlas/common_utils.py
@@ -59,3 +59,52 @@ def site_admin_required(page=False):
         return decorator
     return real_decorator
 
+
+def get_object_in_collection(col, key, debug=False, soft=False):
+    """
+    Gets a record based on key or name from the given pymongo collection
+    """
+    # Find if the key is id
+    obj = None
+    try:
+        obj = col.find_one({'_id' : ObjectId(key)})
+        if obj <> None:
+            return obj
+    except:
+        if debug:
+            print "_ID did not work",
+
+    # else check in the 
+    obj = col.find_one({'name':key})
+
+    if obj <> None:
+        if debug:
+            print 'Name works',
+        return obj
+
+    if soft == True:
+        # dig through the images in the session
+        print "Being softer"
+
+        found = 0
+
+        # Get a list of all image names and 
+        for animage in col.find():
+            if key in animage['name']:
+                print '   Matched :', animage['name']
+                found = found + 1
+                obj = animage
+
+        if found == 1:
+            if debug:
+                print 'Soft matching worked ..'
+            return obj
+
+        elif found > 1:
+            print 'Cannot work with multiple matches ..'
+
+    if debug:
+        print "Nothing worked .."
+    return None
+
+
