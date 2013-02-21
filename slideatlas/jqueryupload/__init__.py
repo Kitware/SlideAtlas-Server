635d1290a2ae0d050389704d37a4ef5bfd0b2fe6
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2013-02-21 11:22:02 -0500

    Upload client requesting id and then pushing the file in chunks

--- slideatlas/jqueryupload/__init__.py
+++ slideatlas/jqueryupload/__init__.py
@@ -1,7 +1,9 @@
 # Expects base.html available to extend from 
 
-from flask import Blueprint, Response, abort, request, session, render_template, current_app, jsonify
+from flask import Blueprint, Response, abort, request, session, render_template, current_app
 from werkzeug import secure_filename
+from bson.objectid import ObjectId
+from slideatlas.common_utils import jsonify
 import os
 import re
 
@@ -11,36 +13,64 @@ mod = Blueprint('upload', __name__,
                 url_prefix="/upload"
                 )
 
-@mod.route('', methods=["GET", "POST"])
+@mod.route('', methods=["GET", "POST", "PUT"])
 def upload():
     """
-    - /tile/4e695114587718175c000006/t.jpg  searches and returns the image
+    Assumes that a chunked upload is provided 
+    A unique gridfs id is created upon first chunk and later   
     """
     if request.method == 'POST':
-        # we are expected to save the uploaded file and return some infos about it:
-        #                              vvvvvvvvv   this is the name for input type=file
-        names = []
-#        return Response("", status=200)
-#        print request.headers
-
-        dummy = request.form
+        # TODO: Get the new method in paramters 
+        # Verfiy that the uploader has access 
+        id = ObjectId()
+        form = request.form
+        # No need to return conventional file list 
         jsonresponse = {}
-        jsonresponse["files"] = []
+        jsonresponse["_id"] = id
+        return jsonify(jsonresponse)
 
-        # Construct a json and send it out as response 
-        obj = {}
-        obj["boundary"] = request.headers["Content-Type"]
-        obj["size"] = False
-        obj["delete_type"] = "DELETE"
-
-        # Parse filename from content disposition 
+    if request.method == 'PUT':
+        # we are expected to save the uploaded file and return some info about it:
+        # this is the name for input type=file
+        names = []
+        # Make sure to read the form before sending the reply
+        # Parse headers 
+        #Get filename from content disposition 
         fnameheader = request.headers["Content-Disposition"]
         disposition = re.search(r'filename="(.+?)"', fnameheader)
         filename = disposition.group(0)[10:-1]
-        obj["delete_url"] = "?file=" + filename
-        obj["name"] = filename
-        print obj
 
+        # Get the actual chunk position from Content-Range
+        range = request.headers["Content-Range"]
+        match = re.findall(r'\d+', range)
+        start = int(match[0])
+        end = int(match[1])
+        total = int(match[2])
+
+        # Expect _id in the form
+        id = request.form['_id']
+
+        # No need to return conventional file list 
+        jsonresponse = {}
+        jsonresponse["done"] = end + 1
+        jsonresponse["_id"] = id
+
+
+        return jsonify(jsonresponse)
+#        print request.headers
+
+
+
+        # Now create the file being uploaded or append etc
+
+        # TODO: Decide the format to return  
+        #        obj = {}
+        #        obj["boundary"] = request.headers["Content-Type"]
+        #        obj["size"] = False
+        #        obj["delete_type"] = "DELETE"
+        #        obj["delete_url"] = "?file=" + filename
+        #        obj["name"] = filename
+        #        print obj
 
         return jsonify(obj)
 
