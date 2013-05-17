0b93747e691dffec02036bec45771062cc2e7946
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2013-05-17 12:35:35 -0400

    Serving websockets too

--- run_websockets.py
+++ run_websockets.py
@@ -1,4 +1,5 @@
 from slideatlas import app
+import sys
 
 # run in under twisted through wsgi
 from twisted.web.wsgi import WSGIResource
@@ -6,8 +7,42 @@ from twisted.web.server import Site
 from twisted.python import log
 from twisted.internet import reactor
 
-resource = WSGIResource(reactor, reactor.getThreadPool(), app)
-site = Site(resource)
+from autobahn.websocket import WebSocketServerFactory, \
+                               WebSocketServerProtocol
 
-reactor.listenTCP(8000, site )
-reactor.run()
+from autobahn.resource import WebSocketResource, HTTPChannelHixie76Aware, WSGIRootResource
+
+
+class EchoServerProtocol(WebSocketServerProtocol):
+
+    def onMessage(self, msg, binary):
+        self.sendMessage(msg, binary)
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) > 1 and sys.argv[1] == 'debug':
+       log.startLogging(sys.stdout)
+       debug = True
+    else:
+       debug = False
+    
+    factory = WebSocketServerFactory("ws://localhost:8000",
+                                     debug = debug,
+                                     debugCodePaths = debug)
+    
+    factory.protocol = EchoServerProtocol
+    factory.setProtocolOptions(allowHixie76 = True) # needed if Hixie76 is to be supported
+    
+    # Mount different resources
+    
+    wsRes = WebSocketResource(factory)
+    
+    wsgiRes = WSGIResource(reactor, reactor.getThreadPool(), app)
+    
+    rootRes = WSGIRootResource(wsgiRes, {'ws' : wsRes})
+    
+    site = Site(rootRes)
+    
+    reactor.listenTCP(8000, site )
+    reactor.run()
