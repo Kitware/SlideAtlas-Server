fc64cefe2a6d3c1c01ff632a882c97ec2f573a44
Dhanannjay (D'jay) Deo <dhanannjay.deo@kitware.com>
2014-03-17 23:53:47 -0400

    Adding ptiff server first example requesting tiles with tms numbering

---
+++ ptiff-server/__init__.py
@@ -0,0 +1,44 @@
+__author__ = 'dhanannjay.deo'
+
+import flask
+from flask import request
+app = flask.Flask(__name__)
+
+@app.route('/')
+def viewer():
+
+    return "Helllo"
+    # flask.send_from_directory("static","index.html")
+
+##############################3
+# for loading tiles
+
+import pymongo
+
+app.config.from_object("config")
+# Connection settings for local demo database for testing (VM)
+conn = pymongo.MongoClient(app.config["MONGO_SERVER"], tz_aware=False, auto_start_request=False)
+admindb = conn["admin"]
+imgdb = conn["demo"]
+colImage = imgdb["531656dea86480a4e608caf9"]
+
+if app.config["LOGIN_REQUIRED"]:
+    admindb.authenticate(app.config["USERNAME"], app.config["PASSWORD"])
+
+@app.route("/tile")
+def tile():
+    # Get variables
+    x = request.args.get('x', 0)
+    y = request.args.get('y', 0)
+    z = request.args.get('z', 0)
+
+    docImage = colImage.find_one({'name': "t.jpg"})
+
+    if docImage == None:
+        flask.abort(403)
+    return flask.Response(str(docImage['file']), mimetype="image/jpeg")
+
+
+
+if __name__ == '__main__':
+    app.run()
