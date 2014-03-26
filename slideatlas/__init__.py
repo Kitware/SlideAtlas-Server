ed4a929e3f67d6720be66158c0d10d4ebca0e145
Brian Helba <brian.helba@kitware.com>
2014-03-26 16:29:53 -0400

    Fix broken links to glviewer and attachments
    Hardcoded relative links are replaced with calls to url_for to the view
    endpoint.

--- slideatlas/__init__.py
+++ slideatlas/__init__.py
@@ -1,15 +1,10 @@
-
-import sys, os
+# coding=utf-8
 
 from slideatlas.core import create_app
 
 # Create App
 app = create_app()
-#sys.path.append(os.path.dirname(__file__))
 
-
-# from celery import Celery
-# celery = Celery(broker="mongodb://127.0.0.1/slideatlas-tasks", backend="mongodb://127.0.0.1/slideatlas-tasks")
 
 # Configure here teh path to put downloaded folders
 # (should be big and with write access to web server user)
