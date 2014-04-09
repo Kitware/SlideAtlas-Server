867c44d5ba6ed675d48c2a7b722a316b6b170013
Brian Helba <brian.helba@kitware.com>
2014-04-09 15:26:14 -0400

    Add 'to_son' method to model classes

--- slideatlas/models/common.py
+++ slideatlas/models/common.py
@@ -1,8 +1,10 @@
 # coding=utf-8
 
+from bson.son import SON
 from mongoengine import register_connection, BooleanField, EmbeddedDocument,\
     EmbeddedDocumentField, ListField, ObjectIdField, StringField
 from mongoengine.connection import get_db
+from mongoengine.base import TopLevelDocumentMetaclass
 from flask.ext.mongoengine import Document, BaseQuerySet
 
 import database  # to prevent circular import problems, this must be an implicit relative import of the whole module
@@ -29,19 +31,89 @@ def register_admin_db(host, dbname, replica_set=None, username=None, password=No
 
 
 ################################################################################
+class ModelQuerySet(BaseQuerySet):
+    def to_son(self, **kwargs):
+        return [document.to_son(**kwargs) for document in self]
+
+
+class ModelDocumentMetaclass(TopLevelDocumentMetaclass):
+    def __init__ (self, name, bases, attrs):
+        # TopLevelDocumentMetaclass has a bug where it doesn't add 'id' to the
+        #   db_field_map of classes that are not derived from another
+        #   non-abstract class
+        self._db_field_map['id'] = '_id'
+        self._reverse_db_field_map['_id'] = 'id'
+        # superclass init is a no-op currently, but make the call to be safe
+        return super(ModelDocumentMetaclass, self).__init__(name, bases, attrs)
+
+
 class ModelDocument(Document):
     """
     A base class for all models.
 
     This uses Flask-MongoEngine as a base, to provide extra convenience methods.
     """
+    __metaclass__ = ModelDocumentMetaclass
+    # DocumentMetaclass has limited functionality if 'my_metaclass' is
+    #   explicitly set to any subclass of DocumentMetaclass, so make it None
+    my_metaclass = None
+
     meta = {
-        'abstract': True
+        'abstract': True,
+        'queryset_class': ModelQuerySet,
     }
+
+    def to_son(self, include_empty=True, exclude_fields=None, only_fields=None):
+        """
+        Return as a SON object.
+
+        The content is identical to the result of 'to_mongo', except that empty
+        fields are included and fields use their model names, instead of
+        database names (e.g. '_id' -> 'id').
+
+        Optionally, if include_empty is True (as it is by default), empty fields
+        are included.
+
+        Optionally, either of 'exclude_fields' or 'only_fields' (but not both)
+        may be specified, containing an iterable of field names to explicitly
+        include or exclude from the result. The 'id' field is always included.
+        """
+        if exclude_fields and only_fields:
+            raise Exception()
+
+        # SON is ordered, so we must build a new one to change keys
+        reverse_db_field_map = dict(self._reverse_db_field_map)
+        reverse_db_field_map['_cls'] = 'type'
+        son = SON( (reverse_db_field_map[mongo_field], value)
+                   for (mongo_field, value)
+                   in self.to_mongo().iteritems() )
+
+        if include_empty:
+            # 'to_mongo' omits empty fields
+            for field_name in self:
+                son.setdefault(field_name, None)
+
+        if exclude_fields:
+            for field in exclude_fields:
+                if field != 'id':
+                    # use 'pop', in case 'include_empty' is False and the field doesn't exist
+                    son.pop(field, None)
+        elif only_fields:
+            only_fields = set(only_fields)
+            only_fields.add('id')  # always include 'id'
+            for field in son.iterkeys():
+                if field not in only_fields:
+                    del son[field]
+
+        return son
+
+
+    # def to_json(self):
+    #     pass
 
 
 ################################################################################
-class MultipleDatabaseQuerySet(BaseQuerySet):
+class MultipleDatabaseModelQuerySet(ModelQuerySet):
     def __init__(self, document, collection):
         # Make a local copy of the Document class for this QuerySet, to prevent database
         # info from being changed once a context manager exists
@@ -51,10 +123,11 @@ class MultipleDatabaseQuerySet(BaseQuerySet):
         # but we need to ensure that its set as a direct property of 'new_document'
         new_document._db_alias = document._db_alias
         new_document._collection = document._collection
-        super(MultipleDatabaseQuerySet, self).__init__(new_document, collection)
+        super(MultipleDatabaseModelQuerySet, self).__init__(new_document, collection)
 
     @staticmethod
     def copy_class(cls):
+        # TODO: move this to a common utils
         return type(cls.__name__, cls.__bases__, dict(cls.__dict__))
 
 
@@ -67,7 +140,7 @@ class MultipleDatabaseModelDocument(ModelDocument):
         'abstract': True,
         'allow_inheritance': False,
         'db_alias': None,  # do not override this in any subclasses
-        'queryset_class': MultipleDatabaseQuerySet,
+        'queryset_class': MultipleDatabaseModelQuerySet,
     }
 
     # TODO: reattach _db_alias to the g object, to make changing it thread-safe
