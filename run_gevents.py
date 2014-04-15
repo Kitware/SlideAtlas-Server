7d477c9e1480fc81fb04703a4d4f56afb2bc6919
Dhanannjay (D'jay) Deo <dhanannjay.deo@kitware.com>
2014-04-14 21:13:48 -0400

    Running using gevents

--- run_gevents.py
+++ run_gevents.py
@@ -3,21 +3,13 @@
 from flask import Flask
 import gevent.wsgi
 import gevent.monkey
-import werkzeug.serving
-
 gevent.monkey.patch_all()
-app = Flask(__name__)
-app.debug = True
 
+import werkzeug.serving
+from slideatlas import app
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-
-@werkzeug.serving.run_with_reloader
 def run_server():
-    ws = gevent.wsgi.WSGIServer(listener=('0.0.0.0', 5000),
+    ws = gevent.wsgi.WSGIServer(listener=('0.0.0.0', 8080),
                                 application=app)
     ws.serve_forever()
 
