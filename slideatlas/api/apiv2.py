2d5c24270a0aef983b733e698387a38ac5201e74
Brian Helba <brian.helba@kitware.com>
2014-06-25 05:38:01 -0400

    Add APIv2 endpoints for ImageStore

--- slideatlas/api/apiv2.py
+++ slideatlas/api/apiv2.py
@@ -320,6 +320,32 @@ class GroupItemAPI(ItemAPI):
 
 
 ################################################################################
+class ImageStoreListAPI(ListAPI):
+    @security.AdminSiteRequirement.protected
+    def get(self):
+        image_stores = models.ImageStore.objects.order_by('label')
+        return jsonify(image_stores=image_stores.to_son(only_fields=('label',)))
+
+    def post(self):
+        abort(501)  # Not Implemented
+
+
+class ImageStoreItemAPI(ItemAPI):
+    @security.AdminSiteRequirement.protected
+    def get(self, image_store):
+        return jsonify(image_stores=[image_store.to_son()])
+
+    def put(self, collection):
+        abort(501)  # Not Implemented
+
+    def patch(self, collection):
+        abort(501)  # Not Implemented
+
+    def delete(self):
+        abort(501)  # Not Implemented
+
+
+################################################################################
 class CollectionListAPI(ListAPI):
     @security.AdminSiteRequirement.protected
     def get(self):
@@ -671,6 +697,16 @@ api.add_resource(GroupListAPI,
 api.add_resource(GroupItemAPI,
                  '/groups/<GroupRole:group>',
                  endpoint='group_item',
+                 methods=('GET', 'PUT', 'PATCH', 'DELETE'))
+
+api.add_resource(ImageStoreListAPI,
+                 '/imagestores',
+                 endpoint='image_store_list',
+                 methods=('GET', 'POST'))
+
+api.add_resource(ImageStoreItemAPI,
+                 '/imagestores/<ImageStore:image_store>',
+                 endpoint='image_store_item',
                  methods=('GET', 'PUT', 'PATCH', 'DELETE'))
 
 api.add_resource(CollectionListAPI,
