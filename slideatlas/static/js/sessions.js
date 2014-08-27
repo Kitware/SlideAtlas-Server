83e852b9cb9f589ffe0543ae1bc48f8361f8e4a8
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2014-08-26 20:10:54 -0400

    Calling the post endpoint and force reloading the list

--- slideatlas/static/js/sessions.js
+++ slideatlas/static/js/sessions.js
@@ -1,16 +1,22 @@
 
-function createNewSession(collectionid) {
+function createNewSession(collectionId, onSuccess) {
     bootbox.prompt({
         title: "Please enter the label for new session:",
         callback: function(value) {
             if(value !== null) {
-                console.log("Collection id: " + collectionid)
+                console.log("Collection id: " + collectionId)
                 console.log("Label enetered: " + value);
-                // $.ajax({
-                //     url: attachmentUrl,
-                //     type: "DELETE",
-                //     success: onSuccess
-                // });
+                $.ajax({
+                    url: "/api/v2/collections/" + collectionId,
+                    type: "POST",
+                    dataType: "json",
+                    contentType: 'application/json;charset=UTF-8',
+                    data: JSON.stringify({ "session" : { "label" : value }, "debug" : 1}, null, "\t"),
+                    success: onSuccess,
+                    error: function () {
+                        console.log("Something went wrong");
+                    }
+                });
             }
             else {
                 console.log("Cancelled ");
