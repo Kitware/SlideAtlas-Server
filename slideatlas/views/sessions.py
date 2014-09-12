62f6786a1e3c51bcb2b9d0ae9e3122fe429d94c1
Brian Helba <brian.helba@kitware.com>
2014-09-12 13:56:53 -0400

    Clean up style and rename local variables in sessions views

--- slideatlas/views/sessions.py
+++ slideatlas/views/sessions.py
@@ -26,10 +26,10 @@ def sessions():
     - /sessions?sessid=10239094124  searches for the session id
     """
     # Support legacy requests for a single session, which use query string args
-    sessid = request.args.get('sessid')
+    session_id = request.args.get('sessid')
 
-    if sessid:
-        session = models.Session.objects.get_or_404(id=sessid)
+    if session_id:
+        session = models.Session.objects.get_or_404(id=session_id)
         return view_a_session(session)
     else:
         return view_all_sessions()
@@ -70,19 +70,20 @@ def view_all_sessions():
                     in sorted(all_sessions.iteritems(), key=lambda item: item[0].label)]
 
     if request.args.get('json'):
-        ajax_sessionlist = [
+        ajax_session_list = [
             {
                 'rule': collection.label,
                 'sessions': [
                     {
                         'sessdb': str(session.image_store.id),
                         'sessid': str(session.id),
-                        'label': session.label}
+                        'label': session.label
+                    }
                     for session, can_admin in sessions],
             }
             for collection, sessions in all_sessions]
         user_name = security.current_user.full_name if security.current_user.is_authenticated() else 'Guest'
-        return jsonify(sessions=ajax_sessionlist, name=user_name, ajax=1)
+        return jsonify(sessions=ajax_session_list, name=user_name, ajax=1)
     else:
         return render_template('sessionlist.html', all_sessions=all_sessions, site_admin=site_admin)
 
@@ -92,29 +93,29 @@ def view_all_sessions():
 @security.ViewSessionRequirement.protected
 def view_a_session(session, next=None):
     # TODO: the template doesn't seem use 'next'
-    next = int(request.args.get('next', 0))
+    next_arg = int(request.args.get('next', 0))
 
     session_son = apiv2.SessionItemAPI._get(session)
 
     if request.args.get('json'):
         ajax_data = {
             'success': 1,
-            'session' : session_son,
-            'images' : [
+            'session': session_son,
+            'images': [
                 {
-                'db': view_son['image_image_store_id'],
-                'img': view_son['image_id'],
-                'label': view_son['label'],
-                'view': view_son['id'],
-                'view_db': view_son['image_store_id'],
-                # 'comparison' is no longer included, but it doesn't appear to have been used
+                    'db': view_son['image_image_store_id'],
+                    'img': view_son['image_id'],
+                    'label': view_son['label'],
+                    'view': view_son['id'],
+                    'view_db': view_son['image_store_id'],
+                    # 'comparison' is no longer included, but it doesn't appear to have been used
                 }
                 for view_son in session_son['views']
             ],
             'attachments': session_son['attachments'],
-            'db' : session.image_store.id, # TODO: deprecate and remove
-            'sessid' : session.id,
-            'next' : url_for('.sessions', sessid=str(session.id), ajax=1, next=next + NUMBER_ON_PAGE)
+            'db': session.image_store.id,  # TODO: deprecate and remove
+            'sessid': session.id,
+            'next': url_for('.sessions', sessid=str(session.id), ajax=1, next=next_arg + NUMBER_ON_PAGE)
         }
         return jsonify(ajax_data)
     else:
@@ -138,39 +139,41 @@ def sessionedit(session):
                            session_son=session_son)
 
 
-def deepcopyview(viewid):
-    if viewid == None :
+################################################################################
+def deepcopyview(view_id):
+    if view_id is None:
         return None
-    admindb = models.ImageStore._get_db()
-    view = admindb['views'].find_one({'_id': ObjectId(viewid)})
-    if view == None :
+    admin_db = models.ImageStore._get_db()
+    view = admin_db['views'].find_one({'_id': ObjectId(view_id)})
+    if view is None:
         return None
     # copy children
-    if view.has_key("Children") :
-        newChildren = []
-        for child in view["Children"] :
+    if 'Children' in view:
+        new_children = []
+        for child in view['Children']:
             new_child = deepcopyview(child)
-            if new_child != None :
-                newChildren.append(new_child)
-        view["Children"] = newChildren
+            if new_child is not None:
+                new_children.append(new_child)
+        view['Children'] = new_children
 
     # this forces a deep copy
     del view['_id']
-    newviewid = admindb['views'].save(view)
-    return newviewid;
+    new_view_id = admin_db['views'].save(view)
+    return new_view_id
 
 
-def deleteview(viewid):
-    admindb = models.ImageStore._get_db()
-    view = admindb['views'].find_one({'_id': ObjectId(viewid)})
-    if view == None :
+################################################################################
+def deleteview(view_id):
+    admin_db = models.ImageStore._get_db()
+    view = admin_db['views'].find_one({'_id': ObjectId(view_id)})
+    if view is None:
         return
     # delete children
-    if view.has_key("Children") :
-        for child in view["Children"] :
+    if 'Children' in view:
+        for child in view['Children']:
             deleteview(child)
     # delete
-    admindb['views'].remove({'_id': viewid})
+    admin_db['views'].remove({'_id': view_id})
 
 
 
@@ -181,23 +184,23 @@ def deleteview(viewid):
 # Use the viewdb var for the source.
 @mod.route('/session-save', methods=['GET', 'POST'])
 def sessionsave():
-    inputStr = request.form['input']  # for post
+    input_str = request.form['input']  # for post
 
-    inputObj = json.loads(inputStr)
-    create_new_session = inputObj["new"]
-    session_id = ObjectId(inputObj["session"])
+    input_obj = json.loads(input_str)
+    create_new_session = input_obj['new']
+    session_id = ObjectId(input_obj['session'])
     # I assume the session uses this to get the thumbnail
-    label = inputObj["label"]
-    view_items = inputObj["views"]
-    hide_annotations = inputObj["hideAnnotation"]
-    stack = inputObj["stack"]
+    label = input_obj['label']
+    view_items = input_obj['views']
+    hide_annotations = input_obj['hideAnnotation']
+    stack = input_obj['stack']
 
     admindb = models.ImageStore._get_db()
 
     # Todo: if session is undefined, create a new session (copy views when available).
-    sessObj = models.Session.objects.with_id(session_id)
+    session = models.Session.objects.with_id(session_id)
 
-    security.EditSessionRequirement(sessObj).test()
+    security.EditSessionRequirement(session).test()
 
     new_views = list()
     for view_item in view_items:
@@ -209,26 +212,24 @@ def sessionsave():
                 view_item['view'] = deepcopyview(ObjectId(view_item['view']))
             # get the view
             view = admindb['views'].find_one({'_id': ObjectId(view_item['view'])})
-
         else:
             # Make a new minimal note / view
             user = security.current_user.full_name if security.current_user.is_authenticated() else 'Guest'
-            viewer_records = []
-            viewer_records.append({
-                'Image'    : ObjectId(view_item['img']),
-                'Database' : ObjectId(view_item['imgdb']),
-            })
+            viewer_records = [{
+                'Image': ObjectId(view_item['img']),
+                'Database': ObjectId(view_item['imgdb']),
+            }]
             view = {
-                'User'          : user,
-                'Type'          : "Note",
-                'SessionId'     : session_id,
-                'ViewerRecords' : viewer_records
+                'User': user,
+                'Type': 'Note',
+                'SessionId': session_id,
+                'ViewerRecords': viewer_records
             }
 
         view.update({
             'Title': view_item['label'],
             'HiddenTitle': view_item['hiddenLabel'],
-        });
+        })
 
         # TODO: don't save until the end, to make failure transactional
         admindb['views'].save(view, manipulate=True)
@@ -237,25 +238,25 @@ def sessionsave():
         # At the moment, the database has many different places to find the db.
         # We will simplify this in the future.
         imgdb = None
-        if view.has_key("db") :
-            # the original legacy oper layers format
-            imgdb = ObjectId(view["db"])
-        else :
+        if 'db' in view:
+            # the original legacy open layers format
+            imgdb = ObjectId(view['db'])
+        else:
             record = view["ViewerRecords"][0]
-            if record.has_key("Database") :
+            if 'Database' in record:
                 # this is the correct location for the image database.
                 # convert references to string to pass to the client
-                imgdb = ObjectId(record["Database"])
-            elif record.has_key("Image") :
-                # A bug caused some image objects to be embedded in views in te databse.
-                imgdb = ObjectId(record["Image"].database);
+                imgdb = ObjectId(record['Database'])
+            elif 'Image' in record:
+                # A bug caused some image objects to be embedded in views in te database.
+                imgdb = ObjectId(record["Image"].database)
 
         new_views.append(models.RefItem(ref=ObjectId(view['_id']),
                                         db=imgdb))
 
     # delete the views that are left over, as views are owned by the session.
-    if not create_new_session :
-        old_view_ids = set(view_ref.ref for view_ref in sessObj.views)
+    if not create_new_session:
+        old_view_ids = set(view_ref.ref for view_ref in session.views)
         new_view_ids = set(view_ref.ref for view_ref in new_views)
 
         removed_view_ids = old_view_ids - new_view_ids
@@ -263,24 +264,24 @@ def sessionsave():
             deleteview(view_id)
 
     # update the session
-    sessObj.label = label  # I am using the label for the annotated title, and name for the hidden title.
-    sessObj.views = new_views
-    sessObj.hide_annotations = bool(hide_annotations)
+    session.label = label  # I am using the label for the annotated title, and name for the hidden title.
+    session.views = new_views
+    session.hide_annotations = bool(hide_annotations)
     # session stacks are going away
-    if stack :
-      sessObj.type = "stack"
-    else :
-      sessObj.type = "session"
+    if stack:
+        session.type = 'stack'
+    else:
+        session.type = 'session'
 
-    if create_new_session :
-        sessObj.user = security.current_user.email
+    if create_new_session:
+        session.user = security.current_user.email
         # TODO: it might be helpful to add a '.clone()' method to 'ModelDocument',
         #  for convenience and to properly set '_created', etc.`
-        sessObj.id = ObjectId()
-        sessObj.save(force_insert=True)
+        session.id = ObjectId()
+        session.save(force_insert=True)
     else:
-        sessObj.save()
-    return jsonify(sessObj.to_mongo())
+        session.save()
+    return jsonify(session.to_mongo())
 
 
 
@@ -288,22 +289,22 @@ def sessionsave():
 @mod.route('/bookmarks')
 @security.login_required
 def bookmarks():
-    rules = []
-
-    id = security.current_user.id
+    user_id = security.current_user.id
 
     # Compile the rules
     # TODO: this is a hack to get a PyMongo admin DB for now, it should be changed
-    admindb = models.ImageStore._get_db()
+    admin_db = models.ImageStore._get_db()
 
-    noteArray = []
+    note_array = []
     # TODO: why is a 'views' collection being saved in the admin DB!!!!
-    for noteObj in admindb["views"].find({"User":ObjectId(id), "Type":"UserNote"}):
-        noteData = {"noteid": str(noteObj["_id"]),
-                    "imgid": noteObj["ViewerRecords"][0]["Image"]["_id"],
-                    "imgdb": noteObj["ViewerRecords"][0]["Image"]["database"],
-                    "title": noteObj["Title"]}
-        noteArray.append(noteData);
+    for note_obj in admin_db['views'].find({'User': user_id, 'Type': 'UserNote'}):
+        note_data = {
+            'noteid': str(note_obj['_id']),
+            'imgid': note_obj['ViewerRecords'][0]['Image']['_id'],
+            'imgdb': note_obj['ViewerRecords'][0]['Image']['database'],
+            'title': note_obj['Title']
+        }
+        note_array.append(note_data)
 
-    data = {"notes": noteArray}
+    data = {'notes': note_array}
     return render_template('notes.html', data=data)
