469cec5bb164e846afaeba9f1c6db5c5cfa822d3
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2014-09-16 16:58:00 -0400

    fixing up openslide reader'

--- slideatlas/ptiffstore/openslide_reader.py
+++ slideatlas/ptiffstore/openslide_reader.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+from base_reader import Reader
 tplpath = os.path.abspath(os.path.join(os.path.dirname(__file__), "tpl"))
 
 # TODO: Make following paths work in windows after openslide is compiled in windows
@@ -13,22 +13,21 @@ _dummylib = cdll.LoadLibrary(openslide_lib)
 import openslide
 
 
-class SlideAtlasImageReader(object):
-
-    def __init__(self):
-        pass
-
-
-class OpenSlideReader(SlideAtlasImageReader):
+class OpenSlideReader(Reader):
 
     def __init__(self):
         super(OpenSlideReader, self).__init__()
 
     def set_input_params(self, params):
         self._reader = openslide.OpenSlide(params['fname'])
+
+        self.width = self._reader.dimensions[0]
+        self.height = self._reader.dimensions[1]
+        self.num_levels = self._reader.level_count
+        self.origin = js["origin"]
+        self.spacing = js["spacing"]
+        self.components = js["components"]
         print self._reader.dimensions
-
-
 
 
 if __name__ == "__main__":
