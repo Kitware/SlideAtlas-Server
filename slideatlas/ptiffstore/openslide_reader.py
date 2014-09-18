d59cb39c4d21c8619445898605c1d063edb275ee
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2014-09-18 12:56:44 -0400

    Removed submodule openslide

--- slideatlas/ptiffstore/openslide_reader.py
+++ slideatlas/ptiffstore/openslide_reader.py
@@ -4,13 +4,6 @@ from base_reader import InvertedReader
 from common_utils import get_max_depth
 tplpath = os.path.abspath(os.path.join(os.path.dirname(__file__), "tpl"))
 
-# TODO: Make following paths work in windows after openslide is compiled in windows
-openslide_lib = os.path.join(tplpath, "openslide", "src", ".libs", "libopenslide.so.0")
-
-from ctypes import cdll
-_dummylib2 = cdll.LoadLibrary("libopenjp2.so.7")
-_dummylib = cdll.LoadLibrary(openslide_lib)
-
 import openslide
 
 __all__ = ("OpenslideReader", )
