0f3dd4300c71e491231d29e10487e51dc2261f9d
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2014-10-13 19:08:39 -0400

    xlimit and proper scaling

--- histogram_images.py
+++ histogram_images.py
@@ -30,18 +30,20 @@ def get_hsv_histograms_2(hsv_image, output_format="svg"):
         bufs = [StringIO.StringIO() for i in range(3)]
 
         h = hsv_image[..., 0].flatten()
-        n_bins = 180
+        n_bins = 360
         hist, edges = np.histogram(h, n_bins)
         fig = plt.figure(figsize=(8, 2.5))
         ax = fig.add_subplot(111)
         hist = normalize(hist.flatten())
-        edges = edges.flatten()
+        edges = edges.flatten() * 2
         colors = [matplotlib.colors.hsv_to_rgb([i / float(n_bins), 1., 1.]) for i in range(n_bins-1)]
-        ax.bar(edges[range(len(hist))], hist, width=1.0, color=colors, edgecolor="none", linewidth=0)
+        ax.bar(edges[range(len(hist))], hist, width=2.0, color=colors, edgecolor="none", linewidth=0)
 
         bincenters = 0.5*(edges[1:] + edges[:-1])
-        ax.plot(bincenters, hist, lw=1., color='black')
-        fig.savefig(bufs[0], format=output_format)
+        bincenters_filtered = bincenters[hist > 0.]
+        hist_filtered = hist[hist > 0.]
+        ax.plot(bincenters_filtered, hist_filtered, lw=1., color='black')
+        fig.savefig(bufs[0], format=output_format, transparent=True)
 
         s = hsv_image[..., 1].flatten()
         n_bins = 256
@@ -52,10 +54,10 @@ def get_hsv_histograms_2(hsv_image, output_format="svg"):
         edges = edges.flatten()
         colors = [matplotlib.colors.hsv_to_rgb([1., i / float(n_bins), 1.]) for i in range(n_bins-1)]
         ax.bar(edges[range(len(hist))], hist, width=1.0, color=colors, edgecolor="none", linewidth=0)
-
+        plt.xlim(xmax=n_bins)
         bincenters = 0.5*(edges[1:] + edges[:-1])
         ax.plot(bincenters, hist, lw=1., color='black')
-        fig.savefig(bufs[1], format=output_format)
+        fig.savefig(bufs[1], format=output_format, transparent=True)
 
         v = hsv_image[..., 2].flatten()
         n_bins = 256
@@ -66,12 +68,12 @@ def get_hsv_histograms_2(hsv_image, output_format="svg"):
         edges = edges.flatten()
         colors = [matplotlib.colors.hsv_to_rgb([1., 0., i / float(n_bins)]) for i in range(n_bins-1)]
         ax.bar(edges[range(len(hist))], hist, width=1.5, color=colors, edgecolor="none", linewidth=0)
-
+        plt.xlim(xmax=n_bins)
         bincenters = 0.5*(edges[1:] + edges[:-1])
         bincenters_filtered = bincenters[hist > 0.]
         hist_filtered = hist[hist > 0.]
         ax.plot(bincenters_filtered, hist_filtered, lw=1., color='black')
-        fig.savefig(bufs[2], format=output_format)
+        fig.savefig(bufs[2], format=output_format, transparent=True)
 
         # fig = plt.figure()
         # ax = plt.subplot(111)
@@ -91,7 +93,7 @@ inp = cv2.imread("/home/dhan/Downloads/download.png")
 # cv2.imwrite('one.png', inp[..., 0])
 # cv2.imwrite('two.png', inp[..., 1])
 # cv2.imwrite('three.png', inp[..., 2])
-output_format = "png"
+output_format = "svg"
 hsv = cv2.cvtColor(inp, cv2.COLOR_BGR2HSV)
 hist_images = get_hsv_histograms_2(hsv, output_format=output_format)
 
