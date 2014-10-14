c6b4f4297101cbb7c5900c0b3086f6a8f3090cc9
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2014-10-14 15:36:08 -0400

    Update button

--- glviewer/static/pluginScarRatio.js
+++ glviewer/static/pluginScarRatio.js
@@ -23,8 +23,29 @@ function pluginScarRatio() {
     // Create a div
     var dialogDiv = $('<div>')
         .attr('id', 'scar_ratio_dialog')
-        .attr('title', 'Color thresholding filter')
+        .attr('title', 'Color thresholding filter');
 
     // Add graphs
-    dialogDiv.dialog();
+    graphs = ['hue', 'saturation', 'value'];
+
+    for(var i = 0; i < graphs.length; i ++) {
+        var chart = $('<img>')
+            .attr('src', '/webgl-viewer/static/temp/' + graphs[i] + ".png")
+            .attr('width','100%')
+            .appendTo(dialogDiv);
+
+        $('<br>').appendTo(dialogDiv);
+
+    }
+
+    $('<button>').appendTo(dialogDiv)
+        .text('Update')
+        .attr('id', 'updateButton');
+
+    var dialog = dialogDiv.dialog();
+
+    $('#updateButton').click(function() {
+        alert("Ready to update !!");
+    });
+
 }
