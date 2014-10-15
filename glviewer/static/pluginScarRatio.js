2f371a0445afb62b4fddcbc3a2d56d10e968fb49
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2014-10-15 16:03:37 -0400

    Updating current values in the filter object

--- glviewer/static/pluginScarRatio.js
+++ glviewer/static/pluginScarRatio.js
@@ -11,7 +11,21 @@ function Filter() {
 };
 
 function FilterColorThreshold() {
-  Filter.call(this);
+    Filter.call(this);
+
+    // Define the selectors
+
+    this.selectors = {};
+    this.selectors['hue'] = {range : [0, 360]};
+    this.selectors['hue'] ={range: [0, 256]};
+    this.selectors['saturation'] = {range: [0, 256]};
+
+    // Current range is the default range
+    for(var key in this.selectors) {
+        if(this.selectors.hasOwnProperty(key)) {
+            this.selectors[key].current = this.selectors[key].range;
+        }
+    }
 };
 
 FilterColorThreshold.prototype = new Filter;
@@ -38,40 +52,58 @@ FilterColorThreshold.prototype.Init = function() {
 }
 
 FilterColorThreshold.prototype.Start = function() {
-
-
-
-
+    var that = this;
     // Create a div
     var dialogDiv = $('<div>')
         .attr('id', 'scar_ratio_dialog')
         .attr('title', 'Color thresholding filter');
 
     // Add graphs
-    var selectors = [ { name: 'hue', max: 360}, { name: 'saturation', max: 256} , {name: 'value', max: 256}];
+    var values = {};
 
-    for(var i = 0; i < selectors.length; i ++) {
-        $('<h3>' + selectors[i].name  + ' </h3>').appendTo(dialogDiv);
+    for(var key in this.selectors) {
+        if(this.selectors.hasOwnProperty(key)) {
+            selector = this.selectors[key];
+            $('<h3>' + key  + ' <span id="value_' + key + '"> </span> </h3>').appendTo(dialogDiv);
 
-        $('<img>')
-            .attr('src', 'data:image/png;base64,' + this.histograms[selectors[i].name])
-            .attr('width','100%')
-            .appendTo(dialogDiv);
+            $('<img>')
+                .attr('src', 'data:image/png;base64,' + this.histograms[key])
+                .attr('width','100%')
+                .appendTo(dialogDiv);
 
-        $('<br>').appendTo(dialogDiv);
-        selectors[i].sliderDiv = $('<div>').appendTo(dialogDiv);
-        selectors[i].sliderDiv.slider({
-            range:true,
-            min: 0,
-            max: selectors[i].max,
-            values: [ 0, selectors[i].max ],
-        });
-
+            $('<br>').appendTo(dialogDiv);
+            selector.sliderDiv = $('<div>').appendTo(dialogDiv);
+            $(selector.sliderDiv).data({key : key});
+            selector.sliderDiv.slider({
+                range:true,
+                min: selector.range[0],
+                max: selector.range[1],
+                values: selector.current,
+                slide: function( event, ui ) {
+                    key = $(event.target).data('key');
+                    selector = that.selectors[key];
+                    selector.current = ui.values;
+                    $('#value_' + key).html('' + JSON.stringify(selector.current));
+                }
+            });
+        }
     }
 
     $('<button>').appendTo(dialogDiv)
         .text('Update')
-        .attr('id', 'updateButton');
+        .attr('id', 'updateButton')
+        .click(function () {
+            alert('About to update');
+            $.ajax({url: '/webgl-viewer/get_mask',
+                data: {
+                    img : VIEWER1.MainView.Canvas[0].toDataURL('image/jpeg')
+                }
+            })
+            .done(function(data) {
+                that.histograms = data;
+                that.Start()
+            });
+        })
 
     $('<br>').appendTo(dialogDiv);
 
