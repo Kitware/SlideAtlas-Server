5b8336dac1b698e599929b94771412ca398782b9
Dhanannjay 'Djay' Deo <dhanannjay.deo@kitware.com>
2015-03-17 02:45:37 -0400

    Automatic filtering through sessions

--- slideatlas/static/ng-app/search/search.js
+++ slideatlas/static/ng-app/search/search.js
@@ -4,30 +4,39 @@ var module = angular.module('SlideAtlas.search', [
     'SlideAtlas.resources'
 ]);
 
-module.controller('SearchCtrl', function ($scope, $location, $http) {
+module.controller('SearchCtrl', function ($scope, $location, $http, filterFilter) {
     $scope.data = {loaded : false};
+    $scope.query = "";
+    $scope.roles = [];
 
-    $http({method: "get", url: "/sessions?json=1"}).
-    success(function(data, status) {
-        $scope.roles = data.sessions;
-        $scope.data.loaded = true;
-    }).
-    error(function(data, status) {
-        console.log("Some error occured while loading sessions")
+    $scope.$watch('query', function ( val ) {
+        for(var i=0; i < $scope.roles.length ; i++) {
+            $scope.roles[i].filtered = filterFilter($scope.roles[i].sessions, $scope.query);
+            $scope.roles[i].isOpen = $scope.roles[i].filtered.length > 0;
+        }
     });
+
+    $http({method: "get", url: "/sessions?json=1"})
+        .success(function(data, status) {
+            $scope.roles = data.sessions;
+            $scope.data.loaded = true;
+        })
+        .error(function(data, status) {
+            console.log("Some error occured while loading sessions")
+        });
 
     $scope.updateSearch = function(term) {
         console.log("Searching :" + term);
+        $scope.query = term;
     };
 
     $scope.commands = {
         'show me *term': function(term) {
           console.log('Show me: ' + term);
-          $scope.$apply($scope.updateSearch);
+          $scope.$apply($scope.updateSearch(term));
         },
         'hey': function() {
           console.log('hey!')
-          $scope.$apply($scope.helloWorld);
         }
     };
 
